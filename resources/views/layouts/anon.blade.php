57e636ee95e194016c125969ea915e02ade74848
sтυx <info@stuxhost.com>
2021-07-08 23:27:38 +0000

    Update resources/views/layouts/admin.blade.php, resources/views/layouts/anon.blade.php, resources/views/layouts/app.blade.php, resources/views/layouts/blank.blade.php, resources/views/layouts/bundle.blade.php, resources/views/site/index.blade.php files

--- resources/views/layouts/anon.blade.php
+++ resources/views/layouts/anon.blade.php
@@ -4,7 +4,7 @@
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
     <meta name="robots" content="noimageindex, noarchive">
     <meta name="mobile-web-app-capable" content="yes">
